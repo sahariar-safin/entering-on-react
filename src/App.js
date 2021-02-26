68375324e4a7af0f9db0d96728201b4eba2cb038
Shafin The Developer <sahariar350@gmail.com>
2021-02-26 11:31:38 +0600

    practice some react function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome!!!!</p>
-      </header>
+      <Counter></Counter>
+      <Users></Users>
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
+}
+
+function Users() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, []);
+  console.log(user);
+  return (
+    <div>
+      <h1>Dynamic user: {user.length}</h1>
+      <ul>
+        {user.map(user => <li>{user.title}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
